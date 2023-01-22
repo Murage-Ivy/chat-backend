f79b25807f1ca7e492b951fa9e3f52cddd97302b
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-01-22 11:24:11 +0300

    Implement jwt methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,35 @@
 class ApplicationController < ActionController::Base
-  def encode_token
-    
+  def encode_token(payload)
+    JWT.encode(payload, ENV["my_secret"])
   end
 
-  def auth
+  def auth_header
+    request.headers["Authorization"]
   end
 
-  def decode_token
+  def decode_token(token)
+    if auth_header
+      token = auth_header.split(" ")[1]
+      begin
+        JWT.decode(token, ENV["my_secret"])
+      rescue JWT::DecodeError
+        nil
+      end
+    end
+  end
+
+  def current_user
+    if decode_token
+      user_id = decode_token[1]["user_id"]
+      user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { errors: ["Please log in"] }, status: :unauthorized unless logged_in?
   end
 end
