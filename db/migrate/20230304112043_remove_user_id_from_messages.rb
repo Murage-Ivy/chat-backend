df5844e2bf7f90ea00ea62f5bdac810a408f3f48
Murage-Ivy <ivymurage2000@gmail.com>
2023-03-04 14:21:58 +0300

    create migrations to remove columns in messages

--- db/migrate/20230304112043_remove_user_id_from_messages.rb
+++ db/migrate/20230304112043_remove_user_id_from_messages.rb
@@ -1,5 +1,5 @@
 class RemoveUserIdFromMessages < ActiveRecord::Migration[7.0]
   def change
-    remove_column :messages, :user_id
+    remove_column :messages, :user_id, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_095710) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_112043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,8 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_095710) do
     t.datetime "updated_at", null: false
     t.integer "reciever"
     t.integer "sender"
-    t.integer "conversation_id"
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
